cb3162135873751c4c418c6018fc129c954ef084
sapanbali <hellosapan4@gmail.com>
2018-08-24 00:59:36 +0530

    second commit

--- app/src/main/java/com/example/sapan/guessthenumber/MainActivity.java
+++ app/src/main/java/com/example/sapan/guessthenumber/MainActivity.java
@@ -13,8 +13,8 @@ import java.util.Random;
 public class MainActivity extends AppCompatActivity {
 
 
-    int rand ;
-
+    int rand;
+    int click = 4;
 
 
     @Override
@@ -24,27 +24,40 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Random random = new Random();
-         rand = random.nextInt(20) + 1;
-
-
-    }
-
-
-public void submit(View view){
-    EditText e1 = (EditText) findViewById(R.id.et);
-    int x = Integer.parseInt(e1.getText().toString());
-
-
-    if (x > rand) {
-        Toast.makeText(MainActivity.this, "Guess lower", Toast.LENGTH_SHORT).show();
-    } else if (x < rand)
-        Toast.makeText(MainActivity.this, "Guess higher", Toast.LENGTH_SHORT).show();
-
-    else {
-        Toast.makeText(MainActivity.this, "Right number!! Guess Again", Toast.LENGTH_SHORT).show();
-        Random random = new Random();
         rand = random.nextInt(20) + 1;
 
+
     }
+
+
+    public void submit(View view) {
+        EditText e1 = (EditText) findViewById(R.id.et);
+        int x = Integer.parseInt(e1.getText().toString());
+
+
+//       int click = 0;
+        click--;
+
+
+        if (click >0) {
+
+            if (x > rand) {
+               Toast.makeText(MainActivity.this, "Guess lower", Toast.LENGTH_SHORT).show();
+            } else if (x < rand)
+                Toast.makeText(MainActivity.this, "Guess higher", Toast.LENGTH_SHORT).show();
+
+            else {
+                Toast.makeText(MainActivity.this, "Right number!! Guess Again", Toast.LENGTH_SHORT).show();
+                Random random = new Random();
+                rand = random.nextInt(20) + 1;
+
+            }
+        }else{
+            Toast.makeText(this,"You are out of try ! number was "+rand ,Toast.LENGTH_SHORT).show();
+            Random random = new Random();
+            rand = random.nextInt(20) + 1;
+            click=4;
+        }
+        Toast.makeText(MainActivity.this, click + "left", Toast.LENGTH_SHORT).show();
     }
 }
